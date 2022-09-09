2665673c35402c967ed57f723df3d0bf63aa9392
Anna_Kryna <krynaann13@gmail.com>
2022-09-09 14:51:13 +0200

    Second commit

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from flask_bootstrap import Bootstrap
 from functools import reduce
 from math import pi
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap(app)
 
 # CREATE DATABASE
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///history.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
